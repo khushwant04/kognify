0537e99cdc854b0a66726bfcf328148983712c0a
khushwant04 <khushwantsanwalot2004@outlook.com>
2024-11-25 15:49:36 +0000

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { TypewriterEffect } from './Typewriter'
+
+
+const Hero = () => {
+  return (
+      <div className='h-screen w-full flex items-center justify-center'>
+      <TypewriterEffect />
+    </div>
+  )
+}
+
+export default Hero
